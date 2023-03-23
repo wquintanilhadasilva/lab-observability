fda0d1f09c8b8b89e41b97297c02759566bdff8a
Wedson Quintanilha da Silva <wedson.silva@oobj.com.br>
2023-03-23 17:33:17 -0300

    fix

--- name-generator-service/entrypoint.sh
+++ name-generator-service/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 echo "Iniciando a aplicação..."
-exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "$@"
+#exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "${HOME}/app.jar" "$@"
+exec filebeat -e -v -c /etc/filebeat/filebeat.yml & exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "app.jar" "$@"
