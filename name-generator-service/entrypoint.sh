f72c5d480573ae295075cc5558f6a1e409693504
Wedson Quintanilha da Silva <wedson.silva@oobj.com.br>
2023-03-24 12:08:32 -0300

    add metricbeat

--- name-generator-service/entrypoint.sh
+++ name-generator-service/entrypoint.sh
@@ -3,4 +3,7 @@
 echo "Iniciando a aplicação..."
 #exec service filebeat start & exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "app.jar" "$@"
 #exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "app.jar" "$@"
-exec filebeat -e -v -c /etc/filebeat/filebeat.yml & exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "app.jar" "$@"
+exec filebeat -e -v -c /etc/filebeat/filebeat.yml & exec java ${JAVA_OPTS} -Djava.security.egd=file:/dev/./urandom -jar "app.jar" "$@" & exec service metricbeat start
+
+
+#exec metricbeat setup -e -E output.logstash.enabled=false -E output.elasticsearch.hosts='[es01:9200'] -E setup.kibana.host=kibana:5601
